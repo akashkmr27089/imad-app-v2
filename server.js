1ae0f960722bcea47463053e191b5b693bf47d2a
akashkmr27089 <akashkmr27089@gmail.com>
2017-02-07 00:02:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('ui/article-one', function(req, res){
+    res.send("Article one served");   
+});
+
+app.get('ui/article-two', function(req,res){
+    res.send("Article two served");
+});
+
+app.get("ui/article-three", function(req,res){
+    res.send("Article three served");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
