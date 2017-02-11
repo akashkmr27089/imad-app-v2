0cdc611e7bd66c4228b7c7e37c2cd1f1a15dc00c
akashkmr27089 <akashkmr27089@gmail.com>
2017-02-11 10:05:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,38 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articletwo={
+    title:'Hello',
+    heading:'Article Two',
+    content:`
+                <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+                 <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+                  <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+`
+}
+fuction Createtemplet(data){
+var title = data.title;
+var content = data.content;
+var heading = data.heading;
+
+var templet=`
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale-1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <h1>There are Two kind of People in this world</h1>
+        <a href='/article-three'>Article three</a>
+        <a href='/article-two'>${heading}</a>
+        <div class="container">
+           ${content}
+        </div>
+    </body>
+</html>`;
+return templet;
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -18,7 +50,7 @@ app.get('/article-one', function(req, res){
 });
 
 app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(Createtemplet(articletwo));
 });
 
 app.get("/article-three", function(req,res){
