233b73141a1986c986fea28b97807c42537ce242
akashkmr27089 <akashkmr27089@gmail.com>
2017-02-11 10:58:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var articletwo={
-    title:'Hello',
-    heading:'Article Two',
-    content:`
-                <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
-                 <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
-                  <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
-`}  ;
+var articles={  //function which has set of data consist of the data and which can be reutnr when asked for it
+    'article-two': {
+        title:'Hello',
+        heading:'Article Two',
+        content:`
+                            <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+                             <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+                              <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+            ` },  //sub  function which works as n array
+    'article-one' : {
+             title:'Hello',
+        heading:'Article One',
+        content:`
+                            <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+                             <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>
+                              <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>`
+        },
+    'article-three' :{
+              title:'Hello',
+        heading:'Article Three',
+        content:`
+                            <p>There are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this worldThere are Two kind of People in this world</p>`
+         }
+};
 
-function createTemplet (data){
+function createTemplet (data){   //data is the variable which will take values from articles the given variable and show tha output
 var title = data.title;
 var content = data.content;
 var heading = data.heading;
@@ -20,7 +36,7 @@ var templet=`
 <html>
     <head>
         <title>${title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale-1" />
+        <meta name="viewport" content="width=device-width, initial-scale-1" />        
          <link href="/ui/style.css" rel="stylesheet" />
     </head>
     <body>
@@ -45,17 +61,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+app.get('/:articlename', function(req,res){   //using : will help us to match the desired article name after the colon with the function --will                                               try to match with tha part and convert that to variable
+    //articlename == article-one which is there in the function
+    //articles[articlename] == {} just like switch it will go to that function and use that if reqquired
+    var articlename = req.params.articlename;  //used to extract article name using by requesting parameter and will compy that to variable articlename 
+    res.send(createTemplet(articles[articlename]));
 });
 
-app.get('/article-two', function(req,res){
-    res.send(createTemplet(articletwo));
-});
-
-app.get("/article-three", function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
